80da420bb55ab65b270b9b46835e339a9cfc87cc
mohammad <bikdli.info@gmail.com>
2024-10-19 21:08:28 +0330

    2.7.0.95

--- Gig.Framework.Apllication.ReadModel/Filters.cs
+++ Gig.Framework.Apllication.ReadModel/Filters.cs
@@ -6,26 +6,7 @@ public static class Filters
 {
     public static IQueryable<T> SetDynamicFilterAndOrder<T>(this IQueryable<T> query, IQuery command)
     {
-        return query; //.WhereClaus(command).OrderByClaus(command);
+        return query; 
     }
 
-    //internal static IQueryable<T> WhereClaus<T>(this IQueryable<T> query, IQueryCommand command)
-    //{
-    //    if (!string.IsNullOrEmpty(command.Where))
-    //    {
-    //        query = query.OrderBy(command.OrderBy);
-    //    }
-
-    //    return query;
-    //}
-
-    //internal static IQueryable<T> OrderByClaus<T>(this IQueryable<T> query, IQueryCommand command)
-    //{
-    //    if (!string.IsNullOrEmpty(command.OrderBy))
-    //    {
-    //        query = query.OrderBy(command.OrderBy);
-    //    }
-
-    //    return query;
-    //}
 }
